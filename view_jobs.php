7a3603168bebc794054605fc1a9945fa1b2cc1b3
Mart <Mart@192.168.192.34>
2014-02-08 21:43:17 +0000

    Updated jobs page html tables

--- view_jobs.php
+++ view_jobs.php
@@ -1,39 +1,40 @@
+<html>
 <head>
-<style>
-table,th,td
-{
-border:1px solid black;
-border-spacing: 0.5rem;
-border-collapse: collapse;
-}
-</style>
+  <link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">
 </head>
 
+</head>
+<body>
+    
+
+    
 <?php
 include 'core/init.php';
 include 'includes/overall/header.php';
+
+echo "<table border =1 >";
+echo "<td>Username</td>";
+echo "<td>Description</td>";
+echo "<td>Budget</td>";
+echo "<td></td>";
 
 $query = mysql_query("SELECT * FROM job")or die(mysql_error());
 while($row2 = mysql_fetch_array($query))
         
 { 
-
- "<ul>";
- echo "Username : ";
- echo $row2['username'];
- echo "<ul>";
- echo "Description: ";
- echo $row2['description'];
- echo "<ul>";
- echo "Budget: ";
- echo $row2['budget']; 
- echo "<ul>";
- echo "Apply: ";
- echo '<a href="job_page.php?job_id='.$row2['job_id'].'">More Information</a>';
- echo "<ul></ul>";
+echo "<tr>";
+echo "<td>" . $row2["username"] . "</td>";
+echo "<td>" . $row2["description"] . "</td>";
+echo "<td>" . $row2["budget"] . "</td>"; 
+echo "<td>".'<a href="job_page.php?job_id='.$row2['job_id'].'">More Information</a>'."</td>";
+ 
+ 
 }
+echo "</table>";
+?>
 
- 
- 
- 
-  ?>
+
+    
+</body>
+
+</html>
