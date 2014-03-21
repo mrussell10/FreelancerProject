da2e507600ba9a0760b37fe16c4064c26d47c571
Mart <Mart@192.168.192.20>
2014-03-21 18:20:45 +0000

    added field for widget in profile and database

--- profile.php
+++ profile.php
@@ -5,64 +5,47 @@ include 'includes/overall/header.php';
 if (isset($_GET['username']) === true && empty($_GET['username']) === false) {
     $username = $_GET['username'];
     $user_id = user_id_from_username($username);
-    $profile_data = user_data($user_id, 'username', 'first_name', 'last_name', 'email', 'address', 'town', 'county', 'overview', 'profile_pic');
+    $profile_data = user_data($user_id, 'username', 'first_name', 'last_name', 'email', 'address', 'town', 'county', 'overview', 'profile_pic','slideshow');
     ?>
 
+    <div id="w">
+        <div id="content" class="clearfix">
+            <div id="userphoto"><?php echo '<img src = "', $profile_data['profile_pic'], '" alt="">'; ?></div>
 
-    <table class="styletable">
-        <th><?php echo $username ?></th>
-        <th>About Me : </th>
-        <tr>
+            <div id="profile_head">
+                <?php echo $profile_data ['username']; ?>
+            </div>
 
-            <td><?php echo '<img src = "', $profile_data['profile_pic'], '" alt="">'; ?>
+            <div id="bio">
+                <?php echo $profile_data ['overview']; ?>
+            </div>
+            
+            <div id="user_widget">
+                <?php echo $profile_data ['slideshow']; ?>
+            </div>
+            <p>
+              
+      
+            <?php
+            $query = 'SELECT `skill_name` FROM `user_skills` WHERE user_id=' . $user_data['user_id'];
+            $result = mysql_query($query) or die(mysql_error());
 
-            <td><?php echo $profile_data ['overview']; ?></td>
-        <tr>
-            <th>Name : </th>
-            <td><?php echo $profile_data ['first_name']; ?>
-                <?php echo $profile_data ['last_name']; ?></td>
-        <tr>
-            <th>Email : </th>
-            <td><?php echo $profile_data ['email']; ?></td>
-        </tr>
-        <tr>
-            <th>Address : </th>
-            <td><?php echo $profile_data ['address']; ?></td>
-        </tr>
-        <tr>
-            <th>Town : </th>
-            <td><?php echo $profile_data ['town']; ?></td>
-        </tr>
-        <tr>
-            <th>County : </th>
-            <td><?php echo $profile_data ['county']; ?></td>
-        </tr>
-        <tr></tr>
+            echo "<table border=1 class=gridtable>";
+            echo "<th> Skills: </th>";
 
+            while ($row = mysql_fetch_array($result)) {
 
-    </tr>
-    </table>
-    <br>
-    <?php
-    $query = 'SELECT `skill_name` FROM `user_skills` WHERE user_id=' . $user_data['user_id'];
-    $result = mysql_query($query) or die(mysql_error());
+                echo "<tr>";
+                echo "<td>" . $row["skill_name"] . "</td>";
+                echo "</tr>";
+            }
 
-    echo "<table border=1 class=gridtable>";
-    echo "<th> Skills: </th>";
+            echo "</table>";
+            ?>
 
-    while ($row = mysql_fetch_array($result)) {
-
-        echo "<tr>";
-        echo "<td>" . $row["skill_name"] . "</td>";
-        echo "</tr>";
-    }
-
-    echo "</table>";
-    ?>
-
-    <?php
-} else {
-    header('location: index.php');
-    exit();
-}
-?>
+            <?php
+        } else {
+            header('location: index.php');
+            exit();
+        }
+        ?>
