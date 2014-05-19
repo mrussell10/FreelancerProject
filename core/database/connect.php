f2ad4dc68e30735c50a562726db4dead71977f6d
Mart <Mart@192.168.192.20>
2014-05-19 21:24:42 +0100

    Revert "* Azure Database  - amend jobs table"
    This reverts commit 29840932dcf3597f8280fd9187e0f6d954a3a2f9.

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,8 +1,8 @@
 <?php
 $connect_error = 'Sorry we are having some connection problems ';
 // Connecting to the server //
-mysql_connect('localhost','root','') or die ($connect_error);
+mysql_connect('us-cdbr-azure-east-a.cloudapp.net','b2ce8924affcde','c9b78b72') or die ($connect_error);
 // Connecting to the freelancer database //
-mysql_select_db('freelancer')or die ($connect_error);
+mysql_select_db('techgigatamisebn')or die ($connect_error);
 
 ?>
